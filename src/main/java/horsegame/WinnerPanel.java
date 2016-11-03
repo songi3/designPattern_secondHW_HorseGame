dfe4c85258849f6a6e56b52c2a54e4314502424c
songi3 <wks4j1004@gmail.com>
2016-11-03 21:03:43 +0900

    WinerPanel 디자인

--- src/main/java/horsegame/WinnerPanel.java
+++ src/main/java/horsegame/WinnerPanel.java
@@ -2,18 +2,18 @@ package horsegame;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class WinnerPanel extends JPanel {
-	JLabel horseLabel;
-	JLabel horseName;
-	ImageIcon horseImage = new ImageIcon("src/images/"+"horse0"+"_win.gif");
 	
+	JLabel horseName;
+	JLabel horseLabel;
+	JLabel ment;
 	public WinnerPanel(Horse horse) {
 		setLayout(null); 
 		setBackground(Color.WHITE);
@@ -22,13 +22,26 @@ public class WinnerPanel extends JPanel {
 		setLocation(80, 50);
 	
 		horseName = new JLabel(horse.getName());
-		horseName.setLocation(400, 200);
-		horseName.setSize(100, 100);
-	
-		horseLabel = new JLabel(horseImage);
-		horseLabel.setLocation(300, 50);
+		horseName.setLocation(370, 200);
+		horseName.setSize(300, 100);
+		Font font1 = new Font("Times",Font.BOLD,25);
+		horseName.setForeground(Color.BLACK);
+		horseName.setFont(font1);
+		
+		ment = new JLabel("W.I.N.N.E.R");
+		ment.setLocation(320, 15);
+		ment.setSize(200, 100);
+		Font font2 = new Font("Times",Font.BOLD,35);
+		ment.setFont(font2);
+		ment.setForeground(Color.BLACK);
+		
+		horseLabel = horse.getHorseLabel();
+		horseLabel.setIcon( new ImageIcon("src/images/"+horseName+"_win.gif"));
+		horseLabel.setLocation(310, 50);
 		horseLabel.setSize(200, 200);
+		
 	
+		add(ment);
 		add(horseName);
 		add(horseLabel);
 	}
