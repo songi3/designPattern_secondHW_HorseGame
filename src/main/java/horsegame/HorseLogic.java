f44105a7fc59a748531210040b34211a3b9ab784
songi3 <wks4j1004@gmail.com>
2016-11-03 22:38:16 +0900

    winnerPanel 연관

--- src/main/java/horsegame/HorseLogic.java
+++ src/main/java/horsegame/HorseLogic.java
@@ -1,12 +1,14 @@
 package horsegame;
 
-import java.awt.BorderLayout;
 import java.util.ArrayList;
+
+import javax.swing.JPanel;
 
 public class HorseLogic extends Thread {
 	ArrayList<Horse> horses;
 	HorsePanel horsePanel;
 	Horse Winner;
+	WinnerPanel winnerPaenl;
 	final int endLine = 795;
 
 	public HorseLogic(ArrayList<Horse> horses, HorsePanel horsePanel) {
@@ -19,9 +21,10 @@ public class HorseLogic extends Thread {
 			for (int i = 0; i < horsePanel.getHorseNumber(); i++) {
 				if (horses.get(i).getX() >= endLine) {
 					Winner = horses.get(i);
+					winnerPaenl = new WinnerPanel(Winner);
 					System.out.println("GameOver");
 					horsePanel.centerPanel.remove();
-					horsePanel.centerPanel.setPanel(new WinnerPanel(Winner));
+					horsePanel.centerPanel.setPanel(winnerPaenl);
 					this.interrupt();
 				}
 			}
